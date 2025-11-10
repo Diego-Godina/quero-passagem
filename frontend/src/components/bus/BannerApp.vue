f87a44cf55cf3c64be60a57fe4c56bf25e695ecb
Diego-Godina <diegosantos.2001@alunos.utfpr.edu.br>
2025-11-10 13:02:07 -0300

    style: BannerApp component

---
+++ frontend/src/components/bus/BannerApp.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="banner-app limit-height">
+    <div class="box-banner-app d-flex justify-content-center">
+      <div>
+        <img src="@/assets/img/banner_download_app.png" alt="Banner para download do app" class="limit-height">
+      </div>
+
+      <a class="link link-android" target="_blank" href="https://play.google.com/store/apps/details?id=br.com.queropassagem" title="Android link"></a>
+      <a class="link link-iphone" target="_blank" href="https://apps.apple.com/br/app/quero-passagem/id1121775076" title="Iphone link"></a>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+  .banner-app {
+    background: #6BCBDB;
+  }
+
+  .limit-height {
+    height: 270px;
+  }
+
+  .box-banner-app {
+    padding: 0 40px;
+  }
+
+  .link {
+    height: 6%;
+    width: 4%;
+    position: absolute;
+    z-index: 2;
+  }
+
+  .link-iphone {
+    left: 52%;
+    top: 155%;
+  }
+
+  .link-android {
+    left: 46%;
+    top: 155%;
+  }
+</style>
--- frontend/src/views/HomeBusTickets.vue
+++ frontend/src/views/HomeBusTickets.vue
@@ -8,6 +8,7 @@
   import FooterPage from '@/components/layouts/footer/FooterPage.vue'
   import FooterCopyright from '@/components/layouts/footer/FooterCopyright.vue'
   import TopDestinies from '@/components/layouts/TopDestinies.vue'
+  import BannerApp from '@/components/bus/BannerApp.vue'
 
 </script>
 
@@ -21,14 +22,16 @@
 
     <BusCompanies/>
 
+    <BannerApp/>
+
     <TopDestinies/>
-
-    <AboveFooter/>
-
-    <FooterPage/>
-
-    <FooterCopyright/>
   </MainContent>
+
+  <AboveFooter/>
+
+  <FooterPage/>
+
+  <FooterCopyright/>
 </template>
 
 <style scoped>
